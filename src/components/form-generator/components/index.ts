b72316dae887b72aabd0d9c8089ac54f15c4e223
s.coulibaly@outlook.com <s.coulibaly@outlook.com>
2025-10-04 20:16:58 +0200

    fix: Suppression de l'import obsolÃ¨te de TrustedProviders

--- src/components/form-generator/components/index.ts
+++ src/components/form-generator/components/index.ts
@@ -2,4 +2,3 @@
 export { FormField } from "./FormField";
 export { FormSection } from "./FormSection";
 export { FormSummary } from "./FormSummary";
-export { TrustedProviders, useTrustedProviders } from "./TrustedProviders"; 
